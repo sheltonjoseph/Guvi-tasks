a6fec204f7c31363519912a1c73f5c0ed5c4366c
shelton013@gmail.com <76867063+sheltonjoseph@users.noreply.github.com>
2021-11-25 03:00:19 +0530

    call back hell

---
+++ call-back-hell/script.js
@@ -0,0 +1,34 @@
+setTimeout(function t10(){
+    document.getElementById("time").innerHTML="10";
+    setTimeout(function t9(){
+        document.getElementById("time").innerHTML="9";
+        setTimeout(function t8(){
+            document.getElementById("time").innerHTML="8";
+            setTimeout(function t7(){
+                document.getElementById("time").innerHTML="7";
+                setTimeout(function t6(){
+                    document.getElementById("time").innerHTML="6";
+                    setTimeout(function t5(){
+                        document.getElementById("time").innerHTML="5";
+                        setTimeout(function t4(){
+                            document.getElementById("time").innerHTML="4";
+                            setTimeout(function t3(){
+                                document.getElementById("time").innerHTML="3";
+                                setTimeout(function t2(){
+                                    document.getElementById("time").innerHTML="2";
+                                    setTimeout(function t1(){
+                                        document.getElementById("time").innerHTML="1";
+                                        setTimeout(function final(){
+                                            document.getElementById("time").innerHTML="HAPPY INDEPENDENCE DAY";
+                                            
+                                            },1000)
+                                        },1000)
+                                    },1000) 
+                                },1000) 
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
